900f89b9932b78c888484ef706441b7f8fa91a8f
kuririne1980 <kuririne1980@gmail.com>
2021-10-04 23:31:53 +0900

    Initial commit

---
+++ app/src/main/java/c/m10/MainActivity.kt
@@ -0,0 +1,30 @@
+package c.m10
+
+import android
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun performFileSearch() {
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+            addCategory(Intent.CATEGORY_OPENABLE)
+            type = "image/*"
+        }
+        startActivityForResult(intent, READ_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, resultData: Intent?) {
+        if (requestCode == READ_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            resultData?.data?.also { uri ->
+                Log.i(TAG, "Uri: $uri")
+                showImage(uri)
+            }
+        }
+    }
+}
